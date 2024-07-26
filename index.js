b71c3735a505aa497af6f6e7a6a81b259918f246
volkinano <147515210+imthematrixl@users.noreply.github.com>
2024-07-26 21:04:55 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+//charAt meth0d
+
+let username = "bro";
+username.charAt(0)
+console.log(username.charAt(0));
+
+
+//indexOF/lastindexOf method
+//let username = "bro"
+//console.log(username.indexOf("o"));
+
+//length
+//console.log(username.length);
+
+
+//trim 
+//let username = "bro"
+//username = username.trim();
+//console.log(username);
+
+//uppercasee/tolowercase
+//username = username.toLower/UpperCase();
+
+//repeat
+//username = username.repeat(1);
+//console.log(username);
+
+//startswith/endswith
+//let username = "bro";
+//let result = username.startsWith(" ");
+//console.log(result);
+
+//includes
+////let username = "bro";
+//let result = username.includes(" ");
+//console.log(result);
+
+let pN = "123-455-7890";
+pN = pN.replaceAll("-", "");
+console.log(pN);
